7807c6ec954911604876a613ebce45e03710829f
my04337 <miuexa@gmail.com>
2019-07-31 19:27:29 +0900

    Update : Sample

--- SimpleMPU.cpp
+++ SimpleMPU.cpp
@@ -13,12 +13,22 @@ using namespace mpu;
 int main()
 {
 	std::cout << ">>> Assembling" << std::endl;
-	Memory mem{0x100};
+	Memory mem{0x80}; // メモリは128バイト
 	Assembler as{mem};
-	as.move(Reg::R0, 1);
-	as.move(Reg::R1, 0x80);
-	as.move(RegIndirect(Reg::R1), 2);
-	as.add(Reg::R0, RegIndirect(Reg::R1), Reg::R2);
+
+	// R0 <= 1
+	as.move(Reg::R0, 1); 
+
+	// R1 <= 0x40
+	as.move(Reg::R1, 0x40); 
+
+	// *R1 <= 2
+	as.move(RegIndirect(Reg::R1), 2); 
+
+	// R2 <= R1 + *R2
+	as.add(Reg::R0, RegIndirect(Reg::R1), Reg::R2); 
+
+	// 終了
 	as.halt();
 
 	std::cout << ">>> Executing" << std::endl;
@@ -26,7 +36,7 @@ int main()
 	mpu.run();
 
 	std::cout << ">>> Result" << std::endl;
-	mem.dump(0, 256);
+	mem.dump(0, 0x80);
 	std::cout << std::endl;
 	mpu.dump();
 	
